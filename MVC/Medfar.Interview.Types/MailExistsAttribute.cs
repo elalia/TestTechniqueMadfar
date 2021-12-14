58346319f438d2e53758fed2f344806f958a860a
elalia <elalia.brenkia.1@etsmtl.net>
2021-12-14 11:03:09 -0500

    test result

---
+++ MVC/Medfar.Interview.Types/MailExistsAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Medfar.Interview.Types
+{
+    public class MailExistsAttribute : ValidationAttribute
+    {
+        private static string _connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=MEDFAR_DEV_INTERVIEW;User id=sa;Password=AliaEts;Integrated Security=True";
+        private static SqlConnection _dbConnection;
+        public override bool IsValid(object value)
+        {            
+            List<string> mail = new List<string>();           
+            mail = GetAllMail();
+            if (mail.FirstOrDefault(m => m == (string)value) == null)
+                return true;
+            else
+                return false;
+        }
+
+        private List<string> GetAllMail()
+        {
+            _dbConnection = new SqlConnection(_connectionString);
+            SqlCommand command = new SqlCommand("SELECT email FROM Users", _dbConnection);
+
+            _dbConnection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<string> emails = new List<string>();           
+            while (reader.Read())
+            {
+                emails.Add(reader["email"].ToString());
+            }
+            return emails;
+        }
+    }
+}
--- MVC/Medfar.Interview.Web/Controllers/ExamController.cs
+++ MVC/Medfar.Interview.Web/Controllers/ExamController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Medfar.Interview.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Medfar.Interview.Types;
+using Medfar.Interview.DAL.Repositories;
 
 namespace Medfar.Interview.Web.Controllers
 {
@@ -11,6 +14,24 @@ namespace Medfar.Interview.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            user.id = Guid.NewGuid();
+            user.date_created = DateTime.Now;
+            UserRepository userinsert = new UserRepository();
+            userinsert.Insert(user);
+
+            //shuffle for fun
+           
+
+            return View();
+
         }
     }
 }
